b4b864358f2980f3d4800c471218e9d2e558b968
Samuel Tamayo Ruiz <samutaru202@gmail.com>
2025-03-16 11:24:40 +0100

    Create Poto.java

---
+++ Poto.java
@@ -0,0 +1,44 @@
+public class Poto<E>{
+    private E[] v;
+    private int tail;
+    private int head;
+    private int size;
+
+
+    public Poto() {
+        this.v = (E[]) new Object[5];
+        head = 0;
+        tail = -1;
+        size = 0;
+    }
+
+
+
+    private int increment(int x) {
+        x++;
+        if (x == v.length) {
+            x = 0;
+        }
+        return x;
+    }
+
+ 
+    private void resize() {
+        E[] newV = (E[]) new Object[v.length * 2]; // Incrementa el tamaño
+        int i = 0;
+        int current = head;
+
+        // Copia los elementos existentes en el nuevo arreglo
+        while (i < size) {
+            newV[i] = v[current];
+            current = increment(current);
+            i++;
+        }
+
+        v = newV; // Asigna el nuevo arreglo
+        head = 0;
+        tail = size - 1;
+    }
+
+   
+}
